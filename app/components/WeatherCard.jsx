28643f0941e350a054af3b1b7537b67b90429443
Datto <76169329+Datto27@users.noreply.github.com>
2022-01-26 22:56:34 +0400

    Update WeatherCard.jsx

--- app/components/WeatherCard.jsx
+++ app/components/WeatherCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { colors } from '../constants/color';
 
-
+// used in SingleCity page
 export default function WeatherCard({weatherData}) {
   // console.log(item.weather[0])
   const weather = weatherData.weather[0] // weather main params
